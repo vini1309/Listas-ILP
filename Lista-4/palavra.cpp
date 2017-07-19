b51ee0544cc5750cd187749714ed331d9e2ebd4f
Vinicius <viniciuspinto@infojr.com.br>
2017-07-19 07:14:08 -0300

    Remove transformação de maiúsculas em minúsculas e corrige o algorítimo da verificação se é palavra ordenada ou não.

--- Lista-4/palavra.cpp
+++ Lista-4/palavra.cpp
@@ -23,17 +23,19 @@ int main () {
 
     for (int i=0; i<numpalavras; i++){
       for (int j=0; j<palavra[i].size(); j++){
-        if(palavra[i][j] >= 'A' && palavra[i][j] <= 'Z')
-          palavra[i][j] += ('a' - 'A');
-      }
-    }
-
-    for (int i=0; i<numpalavras; i++){
-      for (int j=0; j<palavra[i].size(); j++){
-        if (palavra[i][j] > palavra[i][j-1]) continue;
-        else {
-          marcador[i] = 'N'; break;
+        if (palavra[i][j] >= 'A' && palavra[i][j] <= 'Z'
+        && palavra[i][j-1] >= 'a' && palavra[i][j-1] <= 'z'){
+          if (palavra[i][j] > palavra[i][j-1] - ('a'-'A')) continue;
+          else marcador[i] = 'N'; break;
         }
+        else if (palavra[i][j] >= 'a' && palavra[i][j] <= 'z'
+        && palavra[i][j-1] >= 'A' && palavra[i][j-1] <= 'Z'){
+          if (palavra[i][j] > palavra[i][j-1] + ('a'-'A')) continue;
+          else marcador[i] = 'N'; break;
+        }
+        else if (palavra[i][j] > palavra[i][j-1]) continue;
+        else marcador[i] = 'N'; break;
+
       }
     }
 
